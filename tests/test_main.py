e2299767ea2c2b22eebbacd6f73709157fbed4f3
Daniel K. Elder <elder.dk@gmail.com>
2023-03-30 00:22:40 +0900

    add chunk generator test

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import yt_dlp
 
 from whisper_transcribe.main import Transcriber, VideoSource
-from whisper_transcribe.helpers import check_if_valid
+from whisper_transcribe.helpers import check_if_valid, chunk_generator, count_tokens
 
 
 @pytest.fixture
@@ -53,3 +53,13 @@ def test_namedtemporaryfile_deleted(instance):
     instance.__exit__("", "", "")
 
     assert Path(tempfname).is_file() == False
+
+
+def test_chunk_generator(instance):
+    with open("whisper_transcribe/tests/test_data/sample_text.txt", "r") as f:
+        text = f.read()
+
+    result_list = [count_tokens(chunk) for chunk in chunk_generator(text, 2000, 10)]
+    expected_list = [2671, 2518, 2090]
+
+    assert result_list == expected_list
