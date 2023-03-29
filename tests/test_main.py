a4467af45a2861cc8d91b95159e2318a470d7520
Daniel K. Elder <elder.dk@gmail.com>
2023-03-29 13:01:18 +0900

    fix import

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
 import pytest
 from tempfile import NamedTemporaryFile
+from pathlib import Path
+import yt_dlp
 
-from whisper_transcribe.main import Transcriber, VideoSource
+from ..whisper_transcribe.main import Transcriber, VideoSource
+from ..whisper_transcribe.helpers import check_if_valid
 
 
 @pytest.fixture
@@ -30,7 +33,23 @@ def test_determine_source(instance):
     assert instance._determine_source() == VideoSource.UNDETERMINED
 
 
-def test_raise_error_on_undeetermined_source(instance):
-    instance.video_path = "non-existence_video"
+def test_raise_error_on_undetermined_source(instance):
+    instance.video_source = VideoSource.UNDETERMINED
+
     with pytest.raises(ValueError):
-        instance._determine_source()
+        check_if_valid(instance)
+
+
+def test_raise_error_on_invalid_output(instance):
+    instance.output = "invalid_output"
+
+    with pytest.raises(ValueError):
+        check_if_valid(instance)
+
+
+# def test_namedtemporaryfile_deleted(mocker, instance):
+#     mocker.patch("yt_dlp.YoutubeDL.download", return_value="")
+#     fname = instance._download_video()
+#     instance.__exit__()
+
+#     assert Path(fname).is_file() == False
